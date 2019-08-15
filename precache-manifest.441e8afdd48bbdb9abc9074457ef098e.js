242237ac8c652f9767d975e620ed4a11287c2c44
Kalob <kalobcarter@Kalobs-MacBook-Pro.local>
2019-08-15 17:11:57 -0600

    Updates

---
+++ precache-manifest.441e8afdd48bbdb9abc9074457ef098e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f4ffb250e0513dbf92a7f60662b7d6",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "15c3625aff30a84ea1d2",
+    "url": "/clickygame/static/css/main.e9be5096.chunk.css"
+  },
+  {
+    "revision": "11a8e5c11c9cd78c2797",
+    "url": "/clickygame/static/js/2.a54fe88c.chunk.js"
+  },
+  {
+    "revision": "15c3625aff30a84ea1d2",
+    "url": "/clickygame/static/js/main.039d727f.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "243dcd13fb94adc119eb432c4f47e4e3",
+    "url": "/clickygame/static/media/black-felt.243dcd13.png"
+  }
+]);
