f008f88e1320a30ed27a65ba4ef1c95b8aba5e14
Kalob <kalobcarter@Kalobs-MacBook-Pro.local>
2019-08-15 17:03:53 -0600

    removed something that was called but never used

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Instructions from '../Instructions';
 
 function Navbar(props) {
